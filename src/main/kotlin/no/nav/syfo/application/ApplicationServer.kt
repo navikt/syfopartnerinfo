a6ddbcf13217989b1faa28af1fd75749f3f38c1e
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 12:33:52 +0100

    Bump Ktor version
    Bump Ktor version 1.2.5 -> 1.3.2

--- src/main/kotlin/no/nav/syfo/application/ApplicationServer.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationServer.kt
@@ -1,12 +1,11 @@
 package no.nav.syfo.application
 
-import io.ktor.server.engine.ApplicationEngine
-import java.util.concurrent.TimeUnit
+import io.ktor.server.engine.*
 
 class ApplicationServer(private val applicationServer: ApplicationEngine, private val applicationState: ApplicationState) {
     init {
         Runtime.getRuntime().addShutdownHook(Thread {
-            this.applicationServer.stop(10, 10, TimeUnit.SECONDS)
+            this.applicationServer.stop(10, 10)
         })
     }
 
