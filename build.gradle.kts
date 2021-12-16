5bc2161a2d7f28c6ee5b0a2caa6b9d3edbe5c32b
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-12-16 10:26:28 +0100

    Refactor build.gradle
    Place all dependencies in Versions object for simplicity.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,29 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "no.nav.syfo"
 version = "1.0.0"
 
-val coroutinesVersion = "1.4.2"
-val jacksonVersion = "2.11.3"
-val kluentVersion = "1.61"
-val ktorVersion = "1.3.2"
-val logbackVersion = "1.2.3"
-val logstashEncoderVersion = "6.3"
-val prometheusVersion = "0.9.0"
-val spekVersion = "2.0.15"
-val nimbusjosejwtVersion = "9.15.2"
-val spekjunitVersion = "1.1.5"
-val hikariVersion = "3.4.5"
-val jaxbBasicAntVersion = "1.11.1"
-val javaxAnnotationApiVersion = "1.3.2"
-val jaxwsToolsVersion = "2.3.1"
-val jaxbRuntimeVersion = "2.4.0-b180830.0438"
-val javaxJaxwsApiVersion = "2.2.1"
-val jaxbApiVersion = "2.4.0-b180830.0359"
-val javaxActivationVersion = "1.1.1"
-val ojdbc8Version = "19.3.0.0"
-val mockkVersion = "1.10.5"
+object Versions {
+    const val coroutines = "1.4.2"
+    const val hikari = "3.4.5"
+    const val jackson = "2.11.3"
+    const val kluent = "1.61"
+    const val ktor = "1.3.2"
+    const val logback = "1.2.3"
+    const val logstashEncoder = "6.3"
+    const val mockk = "1.10.5"
+    const val nimbusjosejwt = "9.15.2"
+    const val ojdbc8 = "19.3.0.0"
+    const val prometheus = "0.9.0"
+    const val spek = "2.0.15"
+    const val spekjunit = "1.1.5"
+
+    const val javaxAnnotationApi = "1.3.2"
+    const val javaxActivation = "1.1.1"
+    const val javaxJaxwsApi = "2.2.1"
+    const val jaxbApi = "2.4.0-b180830.0359"
+    const val jaxbBasicAnt = "1.11.1"
+    const val jaxbRuntime = "2.4.0-b180830.0438"
+    const val jaxwsTools = "2.3.1"
+}
 
 plugins {
     kotlin("jvm") version "1.4.21"
@@ -45,49 +48,51 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Versions.coroutines}")
 
-    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
+    implementation ("io.ktor:ktor-auth:${Versions.ktor}")
+    implementation ("io.ktor:ktor-auth-jwt:${Versions.ktor}")
+    implementation ("io.ktor:ktor-jackson:${Versions.ktor}")
+    implementation ("io.ktor:ktor-server-netty:${Versions.ktor}")
 
-    implementation ("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation ("io.ktor:ktor-jackson:$ktorVersion")
-    implementation ("io.ktor:ktor-auth:$ktorVersion")
-    implementation ("io.ktor:ktor-auth-jwt:$ktorVersion")
+    // Logging
+    implementation ("ch.qos.logback:logback-classic:${Versions.logback}")
+    implementation ("net.logstash.logback:logstash-logback-encoder:${Versions.logstashEncoder}")
 
-    implementation ("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation ("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+    // Metrics
+    implementation("io.prometheus:simpleclient_hotspot:${Versions.prometheus}")
+    implementation("io.prometheus:simpleclient_common:${Versions.prometheus}")
 
-    implementation ("com.fasterxml.jackson.module:jackson-module-jaxb-annotations:$jacksonVersion")
-    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
-    implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    // (De-)serialization
+    implementation ("com.fasterxml.jackson.module:jackson-module-jaxb-annotations:${Versions.jackson}")
+    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:${Versions.jackson}")
+    implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}")
 
-    implementation("javax.xml.ws:jaxws-api:$javaxJaxwsApiVersion")
-    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationApiVersion")
-    implementation("javax.xml.bind:jaxb-api:$jaxbApiVersion")
-    implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
-    implementation("javax.activation:activation:$javaxActivationVersion")
-    implementation("com.sun.xml.ws:jaxws-tools:$jaxwsToolsVersion") {
-        exclude(group = "com.sun.xml.ws", module = "policy")
+    // Database
+    implementation ("com.oracle.ojdbc:ojdbc8:${Versions.ojdbc8}")
+    implementation ("com.zaxxer:HikariCP:${Versions.hikari}")
+
+    implementation("javax.activation:activation:${Versions.javaxActivation}")
+    implementation("javax.annotation:javax.annotation-api:${Versions.javaxAnnotationApi}")
+    implementation("javax.xml.ws:jaxws-api:${Versions.javaxJaxwsApi}")
+    implementation("javax.xml.bind:jaxb-api:${Versions.jaxbApi}")
+    implementation("org.glassfish.jaxb:jaxb-runtime:${Versions.jaxbRuntime}")
+    implementation("com.sun.xml.ws:jaxws-tools:${Versions.jaxwsTools}") {
+        exclude(group = "com.sun.xml.ws", module = "policy}")
     }
 
-    implementation ("com.zaxxer:HikariCP:$hikariVersion")
-    implementation ("com.oracle.ojdbc:ojdbc8:$ojdbc8Version")
-
-    testImplementation ("io.mockk:mockk:$mockkVersion")
-    testImplementation ("org.amshove.kluent:kluent:$kluentVersion")
-    testImplementation ("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
-    testImplementation ("com.nimbusds:nimbus-jose-jwt:$nimbusjosejwtVersion")
-    testImplementation ("io.ktor:ktor-server-test-host:$ktorVersion")
-
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+    testImplementation ("com.nimbusds:nimbus-jose-jwt:${Versions.nimbusjosejwt}")
+    testImplementation ("io.ktor:ktor-server-test-host:${Versions.ktor}")
+    testImplementation ("io.mockk:mockk:${Versions.mockk}")
+    testImplementation ("org.amshove.kluent:kluent:${Versions.kluent}")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    testRuntimeOnly ("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+    testRuntimeOnly ("org.spekframework.spek2:spek-runner-junit5:${Versions.spek}") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    testRuntimeOnly ("org.jetbrains.spek:spek-junit-platform-engine:$spekjunitVersion")
+    testRuntimeOnly ("org.jetbrains.spek:spek-junit-platform-engine:${Versions.spekjunit}")
 }
 
 
