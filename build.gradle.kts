0bc10882110927a7b3435d7296ebf47dafb0e741
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-03-16 14:12:56 +0100

    Bump Ktor version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ object Versions {
     const val hikari = "5.0.1"
     const val jackson = "2.13.1"
     const val kluent = "1.68"
-    const val ktor = "1.6.7"
+    const val ktor = "1.6.8"
     const val logback = "1.2.10"
     const val logstashEncoder = "7.0.1"
     const val micrometerRegistry = "1.8.2"
