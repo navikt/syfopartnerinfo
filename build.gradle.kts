bc84380169a72e9db2ea28aa7fa82b05b8edec89
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-04-13 10:17:11 +0200

    Bump Kotlin, LogstashEncoder and Micrometer version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ object Versions {
     const val kluent = "1.68"
     const val ktor = "1.6.8"
     const val logback = "1.2.11"
-    const val logstashEncoder = "7.0.1"
-    const val micrometerRegistry = "1.8.3"
+    const val logstashEncoder = "7.1"
+    const val micrometerRegistry = "1.8.4"
     const val mockk = "1.12.3"
     const val nimbusjosejwt = "9.21"
     const val ojdbc8 = "19.3.0.0"
@@ -20,7 +20,7 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
