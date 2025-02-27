1a7466874975150c37a0476bf6a094200632ba86
geir-waagboe <geir.arne.waagbo@nav.no>
2025-02-27 15:01:08 +0100

    Check partnerid

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -48,7 +48,7 @@ fun main() {
         applicationState.ready = true
         application.environment.log.info("Application is ready, running Java VM ${Runtime.version()}")
         try {
-            listOf("146997").forEach {
+            listOf("151295").forEach {
                 val partnerIds = partnerInformasjonService.finnPartnerInformasjon(it)
                 application.environment.log.info(
                     "Partnerids for herId $it is ${
