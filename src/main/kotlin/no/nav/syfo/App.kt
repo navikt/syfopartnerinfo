9ca0221f25ae15783345106f7d7ae8bc0302ad6b
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-10-09 09:52:50 +0200

    IS-3463: Sjekk herid (#89)

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -58,7 +58,7 @@ fun main() {
                 applicationState.ready = true
                 application.environment.log.info("Application is ready, running Java VM ${Runtime.version()}")
                 try {
-                    listOf("151295").forEach {
+                    listOf("109985").forEach {
                         val partnerIds = partnerInformasjonService.finnPartnerInformasjon(it)
                         application.environment.log.info(
                             "Partnerids for herId $it is ${partnerIds.map { it.partnerId }.joinToString(", ")}"
